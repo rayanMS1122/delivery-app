4f697a8577f6dbc15ea03e17ee7b88ccab0c0ca2
Zeeshan Baloch <109962837+zeeshanakhtar012@users.noreply.github.com>
2025-02-13 23:36:14 +0500

    updated screen_utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:delivery_app/screens/all_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart'; // Add this import for GetX
 
 import 'screens/welcome_screen.dart';
@@ -13,10 +14,37 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      // Replace MaterialApp with GetMaterialApp
-      debugShowCheckedModeBanner: false,
-      home: AllScreen(),
-    );
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        // final ThemeData lightTheme = ThemeData(
+        //   useMaterial3: false,
+        //   fontFamily: 'SF Pro Display',
+        //   appBarTheme: AppBarTheme(
+        //     backgroundColor: AppColors.appPrimaryColor,
+        //   ),
+        //   brightness: Brightness.light,
+        //   primaryColor: Colors.white,
+        //   scaffoldBackgroundColor: Colors.white,
+        // );
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          // theme: lightTheme,
+          home: AllScreen(),
+          defaultTransition: Transition.fadeIn,
+          transitionDuration: const Duration(milliseconds: 500),
+          builder: (context, child) {
+            final mediaQueryData = MediaQuery.of(context);
+            final scale = mediaQueryData.textScaleFactor.clamp(0.8, 0.9);
+            return MediaQuery(
+              data: mediaQueryData.copyWith(textScaleFactor: scale),
+              child: child!,
+            );
+          },
+        );
+      },
+    );;
   }
 }
